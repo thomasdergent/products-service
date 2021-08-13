f93be0e10f1ef79ab9933c8e2d79089d1b6120e5
Thomas Dergent <73995291+thomasdergent@users.noreply.github.com>
2021-08-13 23:28:48 +0200

    More secure endpoints

--- src/main/java/com/example/productsservice/controller/ProductController.java
+++ src/main/java/com/example/productsservice/controller/ProductController.java
@@ -19,7 +19,19 @@ public class ProductController {
         return this.productRepository.findAll();
     }
 
-    @GetMapping("/product/store/{storeName}/article/{articleNumber}")
+    @GetMapping("/products/{storeName}")
+    public List<Product> getProductsByStoreName(@PathVariable String storeName) {
+
+        return productRepository.findProductsByStoreName(storeName);
+    }
+
+    @GetMapping("/products/category/{category}")
+    public List<Product> getProductsByCategory(@PathVariable String category) {
+
+        return productRepository.findProductsByCategory(category);
+    }
+
+    @GetMapping("/store/{storeName}/article/{articleNumber}")
     public Product getProductByStoreNameAndArticleNumber(@PathVariable String storeName, @PathVariable String articleNumber) {
         return productRepository.findProductsByStoreNameAndArticleNumber(storeName, articleNumber);
     }
@@ -27,6 +39,7 @@ public class ProductController {
     @PostMapping("/product")
     public Product addProduct(@RequestBody Product product) {
         productRepository.save(product);
+
         return product;
     }
 
@@ -51,9 +64,31 @@ public class ProductController {
         return retrivedProduct;
     }
 
+    @PutMapping("/edit/{storeName}/article/{articleNumber}")
+    public Product edit(@PathVariable String storeName, @PathVariable String articleNumber, @RequestBody Product updatedProduct) {
+        Product retrivedProduct = productRepository.findProductsByStoreNameAndArticleNumber(storeName, articleNumber);
+
+        retrivedProduct.setStoreName(updatedProduct.getStoreName());
+        retrivedProduct.setName((updatedProduct.getName()));
+        retrivedProduct.setCategory((updatedProduct.getCategory()));
+        retrivedProduct.setArticleNumber((updatedProduct.getArticleNumber()));
+        retrivedProduct.setDelivery((updatedProduct.getDelivery()));
+        retrivedProduct.setMaterial((updatedProduct.getMaterial()));
+        retrivedProduct.setMaintenance(updatedProduct.getMaintenance());
+        retrivedProduct.setEnvironment((updatedProduct.getEnvironment()));
+        retrivedProduct.setStock((updatedProduct.getStock()));
+        retrivedProduct.setPrice((updatedProduct.getPrice()));
+        retrivedProduct.setSize((updatedProduct.getSize()));
+
+        productRepository.save(retrivedProduct);
+
+        return retrivedProduct;
+    }
+
     @DeleteMapping("/product/store/{storeName}/article/{articleNumber}")
     public ResponseEntity deleteProduct(@PathVariable String storeName, @PathVariable String articleNumber) {
         Product product = productRepository.findProductsByStoreNameAndArticleNumber(storeName, articleNumber);
+
         if (product != null) {
             productRepository.delete(product);
             return ResponseEntity.ok().build();
--- src/main/java/com/example/productsservice/repository/ProductRepository.java
+++ src/main/java/com/example/productsservice/repository/ProductRepository.java
@@ -4,7 +4,11 @@ import com.example.productsservice.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
     Product findProductsByStoreNameAndArticleNumber(String storeName, String articleNumber);
+    List<Product> findProductsByCategory(String category);
+    List<Product> findProductsByStoreName(String storeName);
 }
